9bae66f9d7f2618b4ef796ef1946f28a059e3865
Rolf <rolf.v.d.forst@fau.de>
2020-02-11 17:32:21 +0100

    Add spl_autoload_register()

--- rrze-cmsinfo.php
+++ rrze-cmsinfo.php
@@ -27,7 +27,22 @@ was zu unerwartetem Verhalten führen kann.
 defined('ABSPATH') || exit;
 
 // Automatische Laden von Klassen.
-require_once __DIR__ . '/vendor/autoload.php';
+spl_autoload_register(function ($class) {
+    $prefix = __NAMESPACE__;
+    $base_dir = __DIR__ . '/includes/';
+
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relativeClass = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relativeClass) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
 
 use RRZE\CMSinfo\Main;
 use RRZE\CMSinfo\Shortcode;
